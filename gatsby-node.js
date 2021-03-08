c8e6270fca3f6056ab6a296a2b504cb23a5ce75c
maturi karthik <karthik.maturi@4d.com>
2021-03-08 01:30:40 +0100

    changing the git config File

--- gatsby-node.js
+++ gatsby-node.js
@@ -21,51 +21,51 @@ exports.onCreatePage = async ({ page, actions }) => {
     }
   }
 
-  const path = require(`path`);
+//   const path = require(`path`);
 
-  // Creating Dynamic pages For Articles
+//   // Creating Dynamic pages For Articles
 
-const makeRequest = (graphql, request) => new Promise((resolve, reject) => {
-  // Query for nodes to use in creating pages.
-  resolve(
-    graphql(request).then(result => {
-      if (result.errors) {
-        reject(result.errors)
-      }
+// const makeRequest = (graphql, request) => new Promise((resolve, reject) => {
+//   // Query for nodes to use in creating pages.
+//   resolve(
+//     graphql(request).then(result => {
+//       if (result.errors) {
+//         reject(result.errors)
+//       }
       
-      return result;
-    })
-  )
-});
+//       return result;
+//     })
+//   )
+// });
 
 // Implement the Gatsby API “createPages”. This is called once the
 // data layer is bootstrapped to let plugins create pages from data.
-exports.createPages = ({ actions, graphql }) => {
-  const { createPage } = actions;
+// exports.createPages = ({ actions, graphql }) => {
+//   const { createPage } = actions;
   
-  const getArticles = makeRequest(graphql, `
-    {
-      allStrapiArticle {
-        edges {
-          node {
-            id
-          }
-        }
-      }
-    }
-    `).then(result => {
-    // Create pages for each article.
-    result.data.allStrapiArticle.edges.forEach(({ node }) => {
-      createPage({
-        path: `/${node.id}`,
-        component: path.resolve(`src/templates/article.js`),
-        context: {
-          id: node.id,
-        },
-      })
-    })
-  });
+//   const getArticles = makeRequest(graphql, `
+//     {
+//       allStrapiArticle {
+//         edges {
+//           node {
+//             id
+//           }
+//         }
+//       }
+//     }
+//     `).then(result => {
+//     // Create pages for each article.
+//     result.data.allStrapiArticle.edges.forEach(({ node }) => {
+//       createPage({
+//         path: `/${node.id}`,
+//         component: path.resolve(`src/templates/article.js`),
+//         context: {
+//           id: node.id,
+//         },
+//       })
+//     })
+//   });
   
-  // Query for articles nodes to use in creating pages.
-  return getArticles;
-};
+//   // Query for articles nodes to use in creating pages.
+//   return getArticles;
+// };
