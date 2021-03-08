a84e7ea1534e616abffd45ff1c10d9ab5c10a766
maturi karthik <karthik.maturi@4d.com>
2021-03-08 01:20:17 +0100

    Correction of error

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,9 +1,12 @@
 import React from 'react'
+import Layout from "../components/layout"
 
 export default function () {
     return (
+       <Layout>
         <div>
             <p>I need to add blog As fast as i can and Correct my login issue.</p>
         </div>
+        </Layout>
     )
 }
